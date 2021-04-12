aa629b23940d5fccc16358dc1c3f1f26a72b09e7
Ahmed Ly <lyahmed1998@gmail.com>
2021-04-12 15:06:59 +0200

    Create s_ahmed.lua

---
+++ s_ahmed.lua
@@ -0,0 +1,39 @@
+time_interval = 1200000
+
+function started(wedf)
+   apiKey = 'GviH7nDAs9XpDSFJs0I6sV5lFTDrriaRqbIR'
+    baseCurrency = 'USD'
+    link[8] = 'https://currencyapi.net/api/v1/rates?key='..apiKey..'&base='..baseCurrency..''
+	print(link[8])
+    fetchRemote (link[8], dataa, "", false,wedf)
+end
+
+
+
+function dataa( responseData, error,wedf)
+    if error == 0 then
+		 local open = fileOpen('data.json')
+         local Write = fileWrite(open,responseData)
+         fileFlush(open)
+         fileClose(open)
+	      end
+end
+ 
+function getUSD ()
+    started("started")
+    local open = fileOpen('data.json')
+    local size = fileGetSize(open)
+    local read = fileRead(open,size)
+    fileFlush(open)
+    fileClose(open)
+    local data = fromJSON(read)
+    return data['rates']['LYD'],data['rates']['SAR'],data['rates']['EGP'],data['rates']['MAD'],data['rates']['AED'],data['rates']['BHD']
+end 
+
+function sendinfo ()
+local LYD,SAR,EGP,MAD,AED,BHD=  getUSD ()
+print("===========================اسعار عملات مقابل الدولار============================================")
+print('LYD='..LYD..'','SAR='..SAR..'',"EGP="..EGP.."","MAD="..MAD.."","AED="..AED.."","BHD="..BHD.."")
+print("===========================اسعار عملات مقابل الدولار============================================")
+end 
+setTimer(sendinfo,time_interval,0)
